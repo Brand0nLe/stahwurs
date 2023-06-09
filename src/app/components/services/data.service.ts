94fe3d87034b9f3e1b7c55e6b465fe75ed9f72c0
Brand0nLe <93brandonle@gmail.com>
2023-06-09 06:03:41 -0700

    modifying fetches and tables

--- src/app/components/services/data.service.ts
+++ src/app/components/services/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, forkJoin, of } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 export interface DataTableItem {
   name: string;
@@ -22,58 +22,35 @@ export class DataService {
           name: person.name,
           field1: person.birth_year,
           field2: person.gender
-        }) as DataTableItem) 
+        }) as DataTableItem)
       ),
-      catchError(() => of([])) 
-    );
-  }
-  
-  fetchHomeworld(url: string): Observable<string> {
-    return this.http.get<{ name: string }>(url).pipe(
-      map(homeworld => homeworld.name),
-      catchError(() => of('Unknown')) 
-    );
-  }
-  
-  fetchSpecies(speciesUrls: string[]): Observable<string> {
-    if (speciesUrls.length === 0) {
-      return of('Unknown');
-    }
-  
-    return forkJoin(
-      speciesUrls.map((url: string) => this.http.get<{ name: string }>(url).pipe(
-        map(species => species.name),
-        catchError(() => of('Unknown')) // Handle error if species fetch fails
-      ))
-    ).pipe(
-      map(speciesNames => speciesNames.join(', '))
+      catchError(() => of([]))
     );
   }
 
-
-
-
-
-
-
-
-  fetchPlanetsData(page: number = 1): Observable<DataTableItem[]> {
-    return this.http.get<{ results: any[] }>(`https://swapi.dev/api/planets/?page=${page}`).pipe(
-      map(({ results }) => results.map(planet => ({
-        name: planet.name,
-        field1: planet.climate,
-        field2: planet.population,
-      })))
+  fetchPlanetsData(): Observable<DataTableItem[]> {
+    return this.http.get<{ results: any[] }>('https://swapi.dev/api/planets/').pipe(
+      map(({ results }) =>
+        results.map((planet: any) => ({
+          name: planet.name,
+          field1: planet.climate,
+          field2: planet.population
+        }) as DataTableItem)
+      ),
+      catchError(() => of([]))
     );
   }
 
-  fetchStarshipsData(page: number = 1): Observable<DataTableItem[]> {
-    return this.http.get<{ results: any[] }>(`https://swapi.dev/api/starships/?page=${page}`).pipe(
-      map(({ results }) => results.map(starship => ({
-        name: starship.name,
-        field1: starship.model,
-        field2: starship.manufacturer,
-      })))
+  fetchStarshipsData(): Observable<DataTableItem[]> {
+    return this.http.get<{ results: any[] }>('https://swapi.dev/api/starships/').pipe(
+      map(({ results }) =>
+        results.map((starship: any) => ({
+          name: starship.name,
+          field1: starship.model,
+          field2: starship.manufacturer
+        }) as DataTableItem)
+      ),
+      catchError(() => of([]))
     );
   }
 }
