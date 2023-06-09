816636a60cf4a2f9b92ad6cf9462727acaa05574
Brand0nLe <93brandonle@gmail.com>
2023-06-09 02:05:34 -0700

    fixed tailwind imports and table imports

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-table',
